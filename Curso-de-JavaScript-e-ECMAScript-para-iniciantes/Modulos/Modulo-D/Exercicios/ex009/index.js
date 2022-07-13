dde22d51a40469d6fa31f3bb65ebe9708411a82a
Emannuel Oliveira Pedreira <emannuelop22@gmail.com>
2022-07-13 10:51:38 -0300

    new

---
+++ Curso-de-JavaScript-e-ECMAScript-para-iniciantes/Modulos/Modulo-D/Exercicios/ex009/index.js
@@ -0,0 +1,15 @@
+var país = 'EUA'
+console.log(`Vivendo em ${país}`)
+if (país == 'Brasil') {
+    console.log('Você é Brasileiro!')
+} else { // Condição composta
+    console.log('Você é Estrangeiro')
+}
+
+/* 
+if (país != 'Brasil') {
+    console.log('Você é Estrangeiro!')
+} else { // Condição composta
+    console.log('Você é Brasileiro')
+}
+*/
